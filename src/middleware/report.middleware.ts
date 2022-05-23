50d8471ead2c28459dd7e1b4cc97f535f7f38a29
SHIROHA <1196768261@qq.com>
2022-05-23 20:48:02 +0800

    #FEAT: fix unused bug

--- src/middleware/report.middleware.ts
+++ src/middleware/report.middleware.ts
@@ -7,7 +7,6 @@ export class ReportMiddleware implements IMiddleware<Context, NextFunction> {
   resolve() {
     return async (ctx: Context, next: NextFunction) => {
       // 控制器前执行的逻辑
-      const startTime = Date.now();
       // 执行下一个 Web 中间件，最后执行到控制器
       // 这里可以拿到下一个中间件或者控制器的返回值
       const result = await next();
